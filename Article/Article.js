0bba06799f5e17cd112ddd69e018d34761e68efe
Guillermo Alfaro <galfarotolon@gmail.com>
2020-04-08 21:01:38 -0500

    Finished MVP with menu items

--- Article/Article.js
+++ Article/Article.js
@@ -201,9 +201,10 @@ articleElements.forEach(articleElement => {
 })
 
 
-
 /*
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page 
   to see the new article.
 
 */
+
+//Done, added in the data array
